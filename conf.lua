8255a7a7808382aa4f6314142a7ebeb5af92e722
yanbentes <yanbentes@protonmail.com>
2022-09-08 13:06:53 -0300

    new icon

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
    t.title = "8 PUZZLE"
    t.version = "11.3"
    t.console = false
-   t.window.icon = "assets/8.png"
+   t.window.icon = "assets/icon.png"
    t.window.width = 400
    t.window.height = 500
    t.window.vsync = 0
